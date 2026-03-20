1c089341fb7b0e4bb3ec18dcbc66d2236c0e1ff8
anujofficial9719-eng <anujofficial9719@gmail.com>
2026-03-20 22:16:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
 
-    API_ID = os.getenv("API_ID", "26649585")
-    API_HASH = os.getenv("API_HASH", "588a3ea6fd01ae88bd2e10fed7d55b2c")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "8102315483:AAEju5xATi7ePCxtJk9rVwmmLiPt-ITd6kY")
-    LOG_CHANNEL_ID = -1002420046638
+    API_ID = os.getenv("API_ID", "34724970")
+    API_HASH = os.getenv("API_HASH", "f240eae7c60e8e30c17203ab0e052f7e")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "8740060958:AAH7zdrLp8U_Jg_dzH5DEyqfWRYyw8WNZAY")
+    LOG_CHANNEL_ID = -1003515041061
